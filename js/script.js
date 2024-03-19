0debe4a1c55bc2baac2163123d99aca01a7da214
Manya Feng <159185413+manya-feng@users.noreply.github.com>
2024-03-19 18:42:12 +0000

    added code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Manya All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Manya
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+
+/**
+ * This function gets users street name and number and shows it back to user.
+ */
+function enterClicked() {
+  // input
+  const streetName = document.getElementById("street-name").value
+  const streetNumber = parseInt(document.getElementById("number-entered").value)
+
+  // output
+  document.getElementById("user-info").innerHTML =
+    "Your address is: " + streetNumber + " " + streetName + "."
+}
